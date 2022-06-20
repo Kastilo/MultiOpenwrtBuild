18edb2203a95e56b3ff1d00beba290ebd75ee8a6
Kastilo <KastiloShepherd@gmail.com>
2022-06-20 15:13:21 -0500

    first build

--- Aurora/files/bin/system-autoupdate.sh
+++ Aurora/files/bin/system-autoupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-OPENVERSION="$(wget -qO- https://api.github.com/repos/Sirherobrine23/OPenwrt-X64/releases | jq -r '.[0].tag_name')"
+OPENVERSION="$(wget -qO- https://api.github.com/repos/Kastilo/OpenWRT/releases | jq -r '.[0].tag_name')"
 [ -z $OPENVERSION ] && exit 0
 URL="https://github.com/Sirherobrine23/WR941NDv3-OpenWRTBuild/releases/download/${OPENVERSION}/"
 # sysupgrade -c "$URL"
